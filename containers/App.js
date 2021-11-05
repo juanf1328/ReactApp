16fcb3c5557894c15ec01d094f93f8ab214c7ac0
juanf1328 <89957661+juanf1328@users.noreply.github.com>
2021-11-05 17:32:19 -0300

    Add files via upload

--- containers/App.js
+++ containers/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css';
-
+import 'tachyons';
 
 class App extends component{
   constructor(){
@@ -27,14 +27,13 @@ componentDidMount(){
 
 
 render(){
-  const {robots, searchfield} = this.state;
-  const filteredRobots = robots.filter(robot =>{
-    return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+  const filteredRobots = this.state.robots.filter(robots =>{
+    return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
   })
-  return !robots.length ?
-    <h1>Loading</h1> :
-  
-  (
+  if(this.state.robots.length===0){
+    return <h1>Loading</h1>
+  }else{
+  return(
     <div className='tc'>
        <h1 className='f1'>Robofriends</h1>
         <SearchBox SearchChange={this.onSearchChange}/>
@@ -45,5 +44,5 @@ render(){
     );
   }
 }
-
+}
 export default App;
