fc6930c7f00bf3508f620f07f13885d8610627a4
juanf1328 <89957661+juanf1328@users.noreply.github.com>
2021-11-08 17:00:31 -0300

    Add files via upload

--- containers/App.js
+++ containers/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import Scroll from './Scroll';
+import ErrorBoundry from '../component/ErrorBoundry';
 import './App.css';
-import 'tachyons';
+
 
 class App extends component{
   constructor(){
@@ -18,7 +19,7 @@ class App extends component{
 componentDidMount(){
   fetch('https://jsonplaceholder.typicode.com/users')
   .then(response=> response.json())
-  .then(users => this.setState({robots:users}));
+  .then(users => {this.setState({robots:users})});
  }
 
   onSearchChange = (event) => {
@@ -27,22 +28,24 @@ componentDidMount(){
 
 
 render(){
-  const filteredRobots = this.state.robots.filter(robots =>{
-    return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-  })
-  if(this.state.robots.length===0){
-    return <h1>Loading</h1>
-  }else{
-  return(
-    <div className='tc'>
-       <h1 className='f1'>Robofriends</h1>
-        <SearchBox SearchChange={this.onSearchChange}/>
-        <Scroll>
-          <CardList robots={filteredRobots} />  
-         </Scroll>
-      </div>
-    );
-  }
+ const {robots, searchfield} = this.state;
+ const filteredRobots = robots.filter(robot=>{
+  return robot.name.toLowerCase().includes(searchfield.toLowerCase)
+ })
+ return !robots.length?
+ <h1>Loading</h1> : 
+ (
+  <div className='tc'>
+   <h1 className='f1'>RoboFriends</h1>
+   <SearchBox searchChange={this.onSearchChange}/>
+   <Scroll>
+<ErrorBoundry>
+   <CardList robots={filteredRobots}/>
+</ErrorBoundry>
+   </Scroll>
+   </div>
+
+  );
 }
 }
 export default App;
